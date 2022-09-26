5021fa7dc4e1039437eda3f5a6db8ee6bd99dd2d
M-Dyeimison <marcosdyeimison@gmail.com>
2022-09-26 14:58:24 -0300

    upgrade

---
+++ Login_Auth/info.py
@@ -0,0 +1,5 @@
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'loginapp878@gmail.com'
+EMAIL_HOST_PASSWORD = 'app102938'
+EMAIL_PORT = 587
--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,10 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
+from Login_Auth import settings
+from django.core.mail import send_mail
 
 # Create your views here.
 def home(request):
@@ -18,6 +20,24 @@ def signup(request):
         pass1 = request.POST['pass1']
         pass2 = request.POST['pass2']
 
+        if User.objects.filter(username=username):
+            messages.error(request, "Username already being used!")
+            return redirect('home')
+
+        if User.objects.filter(email=email):
+            messages.error(request, "Email already registered!")
+            return redirect('home')
+
+        if len(username) > 10:
+            messages.error(request, "Username must be under 10 characters.")
+
+        if pass1 != pass2:
+            messages.error(request, "Passwords didn't match!")
+
+        if not username.isalnum():
+            messages.error(request, "Username must be alpha-numeric!")
+            return redirect('home')
+
         myuser = User.objects.create_user(username, email, pass1)
         myuser.first_name = fname
         myuser.last_name = lname
@@ -25,6 +45,14 @@ def signup(request):
         myuser.save()
 
         messages.success(request, "Your account have been successfully created.")
+
+        # welcomme email
+
+        subject = "Welcome to the (D)jango"
+        message = "Hello " + myuser.first_name + "!! \n" + "\nWelcome to the (D)jango" + "We have sent you a confirmation email, please confirm you email address in order to activate your account."
+        from_email = settings.EMAIL_HOST_USER
+        to_list = [myuser.email]
+        send_mail(subject, message, from_email, to_list, fail_silently=True)
 
         return redirect('signin')
 
@@ -39,9 +67,14 @@ def signin(request):
 
         if user is not None:
             login(request, user)
+            fname = user.first_name
+            return render(request, "authentication/index.html", {'fname': fname})
         else:
             messages.error(request, "Bad Credentials")
+            return redirect('home')
     return render(request, "authentication/signin.html")
 
 def signout(request):
-    pass
+    logout(request)
+    messages.success(request, "Logged Out Successfully!")
+    return redirect('home')
