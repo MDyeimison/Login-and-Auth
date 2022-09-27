ce8767b9efe4c2fc73bf22453f64a9f191100469
M-Dyeimison <marcosdyeimison@gmail.com>
2022-09-26 21:44:30 -0300

    update

--- authentication/urls.py
+++ authentication/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('signup', views.signup, name="signup"),
     path('signin', views.signin, name="signin"),
     path('signout', views.signout, name="signout"),
+    path('activate/<uid64>/<token>', views.activate, name="activate"),
 ]
